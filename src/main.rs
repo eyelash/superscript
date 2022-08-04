46931ebe48405ff2ea027cf9c235432a1f0a6edf
Elias Aebi <eyelash@users.noreply.github.com>
2022-08-04 10:34:25 +0200

    start with the parser

---
+++ src/main.rs
@@ -0,0 +1,169 @@
+mod parser;
+
+use parser::{Parser, optional, repeat, not, peek, sequence, choice, Cursor};
+
+fn any_char(_c: char) -> bool {
+	true
+}
+
+fn skip_comments<'a>(cursor: &mut Cursor<'a>) -> Result<(), Cursor<'a>> {
+	cursor.parse(repeat(char::is_whitespace))?;
+	loop {
+		if let Ok(_) = cursor.parse("/*") {
+			cursor.parse(repeat(sequence!(not("*/"), any_char)))?;
+			cursor.parse("*/")?;
+		} else if let Ok(_) = cursor.parse("//") {
+			cursor.parse(repeat(sequence!(not('\n'), any_char)))?;
+		} else {
+			break;
+		}
+		cursor.parse(repeat(char::is_whitespace))?;
+	}
+	Ok(())
+}
+
+fn identifier_start_char(c: char) -> bool {
+	('a'..='z').contains(&c) || ('A'..='Z').contains(&c) || c == '_'
+}
+fn identifier_char(c: char) -> bool {
+	identifier_start_char(c) || ('0'..='9').contains(&c)
+}
+
+fn parse_identifier<'a>(cursor: &mut Cursor<'a>) -> Result<&'a str, Cursor<'a>> {
+	cursor.parse(sequence!(identifier_start_char, repeat(identifier_char)))
+}
+
+fn keyword(k: &'static str) -> impl Parser {
+	sequence!(k, not(identifier_char))
+}
+
+fn parse_number<'a>(cursor: &mut Cursor<'a>) -> Result<f64, Cursor<'a>> {
+	let s = cursor.parse(repeat('0'..='9'))?;
+	Ok(s.parse().unwrap())
+}
+
+fn parse_toplevel<'a>(cursor: &mut Cursor<'a>) -> Result<(), Cursor<'a>> {
+	if let Ok(_) = cursor.parse(keyword("class")) {
+		skip_comments(cursor)?;
+		parse_identifier(cursor)?;
+		skip_comments(cursor)?;
+		cursor.parse('{')?;
+		skip_comments(cursor)?;
+		cursor.parse('}')?;
+		Ok(())
+	} else if let Ok(_) = cursor.parse(keyword("func")) {
+		skip_comments(cursor)?;
+		parse_identifier(cursor)?;
+		skip_comments(cursor)?;
+		cursor.parse('(')?;
+		skip_comments(cursor)?;
+		while let Ok(_) = cursor.parse(not(')')) {
+			parse_identifier(cursor)?;
+			skip_comments(cursor)?;
+			match cursor.parse(',') {
+				Ok(_) => {
+					skip_comments(cursor)?;
+					continue
+				}
+				Err(_) => break
+			}
+		}
+		cursor.parse(')')?;
+		skip_comments(cursor)?;
+		cursor.parse('{')?;
+		skip_comments(cursor)?;
+		cursor.parse('}')?;
+		Ok(())
+	} else {
+		cursor.error()
+	}
+}
+
+fn parse_file<'a>(cursor: &mut Cursor<'a>) -> Result<(), Cursor<'a>> {
+	skip_comments(cursor)?;
+	while let Ok(_) = cursor.parse(peek(any_char)) {
+		parse_toplevel(cursor)?;
+		skip_comments(cursor)?;
+	}
+	Ok(())
+}
+
+struct Bold<T>(T);
+
+impl <T: std::fmt::Display> std::fmt::Display for Bold<T> {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+		write!(f, "\x1B[1m{}\x1B[22m", self.0)?;
+		Ok(())
+	}
+}
+
+fn bold<T: std::fmt::Display>(t: T) -> Bold<T> {
+	Bold(t)
+}
+
+struct Red<T>(T);
+
+impl <T: std::fmt::Display> std::fmt::Display for Red<T> {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+		write!(f, "\x1B[31m{}\x1B[39m", self.0)?;
+		Ok(())
+	}
+}
+
+fn red<T: std::fmt::Display>(t: T) -> Red<T> {
+	Red(t)
+}
+
+struct Green<T>(T);
+
+impl <T: std::fmt::Display> std::fmt::Display for Green<T> {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+		write!(f, "\x1B[32m{}\x1B[39m", self.0)?;
+		Ok(())
+	}
+}
+
+fn green<T: std::fmt::Display>(t: T) -> Green<T> {
+	Green(t)
+}
+
+fn print_error<W: std::io::Write>(cursor: &Cursor, mut write: W) -> std::io::Result<()> {
+	writeln!(write, "{}:", bold(red("error")))?;
+	let mut start = 0;
+	let mut end = cursor.s.len();
+	let mut num = 0;
+	for (i, c) in cursor.s.char_indices() {
+		if c == '\n' {
+			if i < cursor.i {
+				start = i + c.len_utf8();
+				num += 1;
+			} else {
+				end = i;
+				break;
+			}
+		}
+	}
+	let line = cursor.s.get(start..end).unwrap();
+	writeln!(write, "{} | {}", num, line)?;
+	write!(write, "{} | ", num)?;
+	for (_, c) in line.char_indices().take_while(|(i, _)| start + *i < cursor.i) {
+		let c = if c.is_whitespace() { c } else { ' ' };
+		write!(write, "{}", c)?;
+	}
+	writeln!(write, "^")?;
+	Ok(())
+}
+
+fn main() {
+	match std::env::args().nth(1) {
+		Some(arg) => {
+			let file = std::fs::read_to_string(arg).unwrap();
+			let mut cursor = Cursor::new(file.as_str());
+			match parse_file(&mut cursor) {
+				Ok(()) => println!("{}", bold(green("success"))),
+				Err(c) => print_error(&c, std::io::stderr().lock()).unwrap(),
+			}
+		},
+		None => eprintln!("{}: no input file", bold(red("error"))),
+	}
+}
---
+++ src/parser.rs
@@ -0,0 +1,204 @@
+pub trait Parser {
+	fn parse(&mut self, s: &str) -> Option<usize>;
+}
+
+impl Parser for char {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		match s.chars().next() {
+			Some(c) if *self == c => Some(c.len_utf8()),
+			_ => None,
+		}
+	}
+}
+
+impl Parser for std::ops::RangeInclusive<char> {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		match s.chars().next() {
+			Some(c) if self.contains(&c) => Some(c.len_utf8()),
+			_ => None,
+		}
+	}
+}
+
+impl <F: FnMut(char) -> bool> Parser for F {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		match s.chars().next() {
+			Some(c) if self(c) => Some(c.len_utf8()),
+			_ => None,
+		}
+	}
+}
+
+impl Parser for &str {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		if s.starts_with(*self) {
+			Some(self.len())
+		} else {
+			None
+		}
+	}
+}
+
+struct Optional<P: Parser>(P);
+
+impl <P: Parser> Parser for Optional<P> {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		match self.0.parse(s) {
+			Some(i) => Some(i),
+			None => Some(0),
+		}
+	}
+}
+
+pub fn optional<P: Parser>(p: P) -> impl Parser {
+	Optional(p)
+}
+
+struct Repetition<P: Parser>(P);
+
+impl <P: Parser> Parser for Repetition<P> {
+	fn parse(&mut self, mut s: &str) -> Option<usize> {
+		let mut sum = 0;
+		while let Some(len) = self.0.parse(s) {
+			s = s.split_at(len).1;
+			sum += len;
+		}
+		Some(sum)
+	}
+}
+
+pub fn repeat<P: Parser>(p: P) -> impl Parser {
+	Repetition(p)
+}
+
+struct Not<P: Parser>(P);
+
+impl <P: Parser> Parser for Not<P> {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		match self.0.parse(s) {
+			Some(_) => None,
+			None => Some(0),
+		}
+	}
+}
+
+pub fn not<P: Parser>(p: P) -> impl Parser {
+	Not(p)
+}
+
+struct Peek<P: Parser>(P);
+
+impl <P: Parser> Parser for Peek<P> {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		match self.0.parse(s) {
+			Some(_) => Some(0),
+			None => None,
+		}
+	}
+}
+
+pub fn peek<P: Parser>(p: P) -> impl Parser {
+	Peek(p)
+}
+
+struct FunctionParser<F>(F);
+
+impl <F: Fn(&mut Cursor) -> Option<()>> Parser for FunctionParser<F> {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		let mut cursor = Cursor::new(s);
+		match self.0(&mut cursor) {
+			Some(()) => Some(cursor.i),
+			None => None,
+		}
+	}
+}
+
+pub fn from_function<F: Fn(&mut Cursor) -> Option<()>>(f: F) -> impl Parser {
+	FunctionParser(f)
+}
+
+struct Sequence<P0, P1>(P0, P1);
+
+impl <P0: Parser, P1: Parser> Parser for Sequence<P0, P1> {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		let len0 = self.0.parse(s)?;
+		let (_, s) = s.split_at(len0);
+		let len1 = self.1.parse(s)?;
+		Some(len0 + len1)
+	}
+}
+
+pub fn sequence0<P0: Parser, P1: Parser>(p0: P0, p1: P1) -> impl Parser {
+	Sequence(p0, p1)
+}
+macro_rules! sequence {
+	($p0:expr, $($p:expr),+) => {
+		$crate::parser::sequence0($p0, $crate::parser::sequence!($($p),+))
+	};
+	($p:expr) => {
+		$p
+	};
+}
+pub(crate) use sequence;
+
+struct Choice<P0, P1>(P0, P1);
+
+impl <P0: Parser, P1: Parser> Parser for Choice<P0, P1> {
+	fn parse(&mut self, s: &str) -> Option<usize> {
+		if let Some(len) = self.0.parse(s) {
+			return Some(len);
+		}
+		if let Some(len) = self.1.parse(s) {
+			return Some(len);
+		}
+		None
+	}
+}
+
+pub fn choice0<P0: Parser, P1: Parser>(p0: P0, p1: P1) -> impl Parser {
+	Choice(p0, p1)
+}
+macro_rules! choice {
+	($p0:expr, $($p:expr),+) => {
+		$crate::parser::choice0($p0, $crate::parser::choice!($($p),+))
+	};
+	($p:expr) => {
+		$p
+	};
+}
+pub(crate) use choice;
+
+pub fn parse<'a, P: Parser>(mut p: P, s: &'a str) -> Option<(&'a str, &'a str)> {
+	p.parse(s).map(|i| s.split_at(i))
+}
+
+pub struct Cursor<'a> {
+	pub s: &'a str,
+	pub i: usize,
+}
+
+impl <'a> Cursor<'a> {
+	pub fn new(s: &'a str) -> Self {
+		Cursor {
+			s,
+			i: 0,
+		}
+	}
+	pub fn error<T>(&self) -> Result<T, Cursor<'a>> {
+		Err(Cursor {
+			s: self.s,
+			i: self.i,
+		})
+	}
+	pub fn parse<P: Parser>(&mut self, mut p: P) -> Result<&'a str, Cursor<'a>> {
+		let (_, s) = self.s.split_at(self.i);
+		match p.parse(s) {
+			Some(i) => {
+				self.i += i;
+				let (result, _) = s.split_at(i);
+				Ok(result)
+			},
+			None => self.error(),
+		}
+	}
+}
